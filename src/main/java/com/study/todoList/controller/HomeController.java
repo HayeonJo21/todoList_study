51311053d2ea811da2670957d3d0dcc5ed09136c
HayeonJo21 <gkdus6629@gmail.com>
2022-09-02 10:04:38 +0900

    HomeController 작성 및 application.properties 내용 작성

--- src/main/java/com/study/todoList/controller/HomeController.java
+++ src/main/java/com/study/todoList/controller/HomeController.java
@@ -1,8 +1,19 @@
 package com.study.todoList.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
-    
+    @GetMapping("/")
+    public ModelAndView home(Model model, HttpSession session) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");
+
+        return mav;
+    }
 }
